612c5bad4fda7fea0356af15100ee6d2747d8556
adilarshidin <adil.therunningman@gmail.com>
2025-11-25 12:04:55 +0100

    fix: filters object type

--- part2/2.6-2.10/src/App.jsx
+++ part2/2.6-2.10/src/App.jsx
@@ -3,40 +3,22 @@ import { useState } from 'react';
 import uuid from './utils/uuid_generator';
 import Persons from './components/Persons';
 import PersonAdditionForm from './components/PersonAdditionForm';
+import Filter from './components/Filter';
 
 
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'John Doe', id: uuid(), phone: '1234567890' }
   ]);
-  const [filters, setFilters] = useState([
+  const [filters, setFilters] = useState(
     { useFilters: false, name: '' }
-  ]);
+  );
   const [filteredName, setFilteredName] = useState('');
-
-  const handleFilteredNameInput = (event) => {
-    const newFilteredName = event.target.value;
-    const newFilters = filters;
-
-    if (!newFilteredName) {
-      filters.useFilters = false;    
-    } else {
-      filters.useFilters = true;
-    };
-
-    filters.name = newFilteredName;
-    setFilteredName(newFilteredName);
-    setFilters(newFilters);
-  };
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-        <div>
-          <p>Find person by name: <input value={filteredName} onChange={handleFilteredNameInput} /></p>
-        </div>
-      </div>
+      <Filter filters={filters} filteredName={filteredName} setFilteredName={setFilteredName} setFilters={setFilters} />
       <PersonAdditionForm persons={persons} setPersons={setPersons} />
       <h2>Numbers</h2>
       <ul>
