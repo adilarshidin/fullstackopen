32ab721ff8735a19a82caba81cbb5d093e588d4d
adilarshidin <adil.therunningman@gmail.com>
2025-12-13 18:06:40 +0100

    feat: 7.7

---
+++ part7/7.7/src/App.jsx
@@ -0,0 +1,86 @@
+import { useState, useEffect } from 'react'
+
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [country, setCountry] = useState(null);
+
+  useEffect(() => {
+    const getCountry = async () => {
+      const response = await fetch(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`);
+      setCountry(await response.json());
+    };
+    getCountry();
+  }, [name])
+
+  if(!country || country.error) {
+    return { found: false };
+  } else if (!country.error) return {
+    data: {
+      name: country.name.common,
+      capital: country.capital[0],
+      population: country.population,
+      flag: country.flags.png
+    },
+    found: true };
+};
+
+const Country = ({ country }) => {
+  if (!country) {
+    return null
+  }
+
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.data.name} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div> 
+      <img src={country.data.flag} height='100' alt={`flag of ${country.data.name}`}/>  
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App;
