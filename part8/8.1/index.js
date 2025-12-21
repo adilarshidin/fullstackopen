faeda5b9e34b99e694659ecd292d0d43793982e9
adilarshidin <adil.therunningman@gmail.com>
2025-12-21 11:56:46 +0100

    feat: 8.4

--- part8/8.1/index.js
+++ part8/8.1/index.js
@@ -116,7 +116,7 @@ const typeDefs = /* GraphQL */ `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -125,7 +125,11 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author) return books
+      const byAuthor = (book) => args.author === book.author ? book.author : !book.author
+      return books.filter(byAuthor)
+    },
     allAuthors: () => authors
   },
   Author: {
