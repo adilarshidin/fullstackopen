8999b58bfe67cf14f641cbc85266c7c553d87079
adilarshidin <adil.therunningman@gmail.com>
2025-12-21 12:44:29 +0100

    feat: 8.6

--- part8/8.1/index.js
+++ part8/8.1/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid');
 
 let authors = [
   {
@@ -119,6 +120,15 @@ const typeDefs = /* GraphQL */ `
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -144,6 +154,16 @@ const resolvers = {
     bookCount: (parent) => {
       const authorBooks = books.filter(book => book.author === parent.name)
       return authorBooks.length
+    }
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      if (!authors.find(author => author.name === args.author)) {
+        authors.push({ name: args.author })
+      }
+      books.push(book)
+      return book
     }
   }
 }
