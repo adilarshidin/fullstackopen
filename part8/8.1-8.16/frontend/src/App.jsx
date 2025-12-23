45a728d0c58bda1ba3f24da599e042790bca00ed
adilarshidin <adil.therunningman@gmail.com>
2025-12-23 16:03:54 +0100

    feat: 8.25

--- part8/8.1-8.16/frontend/src/App.jsx
+++ part8/8.1-8.16/frontend/src/App.jsx
@@ -3,15 +3,33 @@ import { useQuery, useApolloClient, useSubscription } from "@apollo/client/react
 
 import ViewRoutes from "./ViewRoutes";
 import Menu from "./components/Menu";
-import { BOOK_ADDED } from "./queries"
+import { BOOK_ADDED, ALL_BOOKS } from "./queries"
+
+const updateCache = (cache, query, addedBook) => {
+  const uniqueByName = (book) => {
+    let seen = new Set();
+    return book.filter((item) => {
+      let key = item.title
+      return seen.has(key) ? false : seen.add(key)
+    })
+  }
+
+  cache.updateQuery(query, ({ allBooks }) => {
+    return {
+      allBooks: uniqueByName(allBooks.concat(addedBook))
+    }
+  })
+}
 
 const App = () => {
+  const client = useApolloClient()
   const [token, setToken] = useState(() => localStorage.getItem("token"));
 
   useSubscription(BOOK_ADDED, {
     onData: ({ data }) => {
       const addedBook = data.data.bookAdded
       alert(`${JSON.stringify(addedBook)} added`)
+      updateCache(client.cache, { query: ALL_BOOKS }, addedBook)
     }
   })
 
