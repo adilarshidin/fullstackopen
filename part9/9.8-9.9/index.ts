ae9533f312f1891062054660ccd1d482f2aae8d1
adilarshidin <adil.therunningman@gmail.com>
2025-12-27 13:35:19 +0100

    feat: 9.10

--- part9/9.8-9.9/index.ts
+++ part9/9.8-9.9/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import { Response } from "express";
 import cors from "cors";
+
+import { Diagnosis } from "./types";
+import data from "./data/diagnoses";
 
 const app = express();
 app.use(cors());
@@ -10,6 +14,10 @@ app.get('/api/ping', (_req, res) => {
 
 app.get('/api/patients', (_req, res) => {
   res.send({ "patient1": { "id": "1", "name": "guy", "gender": "m", "occupation": "dunno" } });
+});
+
+app.get('/api/diagnoses', (_req, res: Response<Diagnosis[]>) => {
+  res.send(data);
 });
 
 const PORT = 3001;
